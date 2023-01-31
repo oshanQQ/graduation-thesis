4dcdacbba2249e78552f3540d1269dff978ae9df
oshanQQ <oshanqq@gmail.com>
2023-01-31 15:15:47 +0900

    Provisioner could be deployed

--- kubernetes/nfs/deploy-provisioner.sh
+++ kubernetes/nfs/deploy-provisioner.sh
@@ -1,7 +1,8 @@
 helm upgrade --cleanup-on-fail \
-    nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --namespace nfs \
+    --create-namespace \
     --set nfs.server=192.168.10.51 \
     --set nfs.path=/nfs \
-    --set nfs.mountOptions=v4 \
     --set storageClass.defaultClass=true \
     --set storageClass.name=nfs
